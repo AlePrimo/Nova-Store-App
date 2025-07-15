8158a535e02d9b4118462f1424fe1db813ace22b
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-07-14 21:09:19 -0300

    Cambio de nombre en clases test

--- src/test/java/com/aleprimo/nova_store/Nova_StoreTests.java
+++ src/test/java/com/aleprimo/nova_store/Nova_StoreTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PlantillaBackendApplicationTests {
+class Nova_StoreTests {
 
 	@Test
 	void contextLoads() {
