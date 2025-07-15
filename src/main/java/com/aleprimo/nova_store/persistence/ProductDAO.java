f92fac077b5c0ca1cf249bc74c65a0b4c85cda09
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-07-14 21:59:49 -0300

    Agregado de ProductDAO y ProductDAOImpl

---
+++ src/main/java/com/aleprimo/nova_store/persistence/ProductDAO.java
@@ -0,0 +1,24 @@
+package com.aleprimo.nova_store.persistence;
+
+import com.aleprimo.nova_store.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+
+public interface ProductDAO {
+
+    Page<Product> findAllProducts(Pageable pageable);
+
+    Page<Product> findActiveProducts(Pageable pageable);
+
+    Page<Product> findByNameContaining(String name, Pageable pageable);
+
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
+
+    Page<Product> findByCategoryIdAndNameContaining(Long categoryId, String name, Pageable pageable);
+
+    Optional<Product> findBySku(String sku);
+
+}
---
+++ src/main/java/com/aleprimo/nova_store/persistence/implementations/ProductDAOImpl.java
@@ -0,0 +1,50 @@
+package com.aleprimo.nova_store.persistence.implementations;
+
+import com.aleprimo.nova_store.models.Product;
+import com.aleprimo.nova_store.persistence.ProductDAO;
+import com.aleprimo.nova_store.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+@RequiredArgsConstructor
+public class ProductDAOImpl implements ProductDAO {
+
+    private final ProductRepository productRepository;
+
+
+    @Override
+    public Page<Product> findAllProducts(Pageable pageable) {
+        return this.productRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Product> findActiveProducts(Pageable pageable) {
+        return this.productRepository.findByIsActiveTrue(pageable);
+    }
+
+    @Override
+    public Page<Product> findByNameContaining(String name, Pageable pageable) {
+        return this.productRepository.findByNameContainingIgnoreCase(name, pageable);
+    }
+
+    @Override
+    public Page<Product> findByCategoryId(Long categoryId, Pageable pageable) {
+        return this.productRepository.findByCategoryId(categoryId, pageable);
+    }
+
+    @Override
+    public Page<Product> findByCategoryIdAndNameContaining(Long categoryId, String name, Pageable pageable) {
+        return this.productRepository.findByCategoryIdAndNameContainingIgnoreCase(categoryId, name, pageable);
+    }
+
+    @Override
+    public Optional<Product> findBySku(String sku) {
+        return this.productRepository.findBySku(sku);
+    }
+}
