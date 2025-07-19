288193bb74861ae9255dae4d4fbecac4f2862555
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-07-19 19:42:54 -0300

    Agregado de todos los atributos para clases Order y OrderItem

--- src/main/java/com/aleprimo/nova_store/models/Order.java
+++ src/main/java/com/aleprimo/nova_store/models/Order.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,14 +22,23 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
     Customer customer;
     @Column(name = "order_items")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     List<OrderItem> orderItems = new ArrayList<>();
-    @Column(name = "total_amount")
-    Double totalAmount;
-
-
-
-
+    @Column(name = "total_amount", nullable = false)
+    BigDecimal totalAmount;
+    @Column(nullable = false)
+    String paymentMethod;
+    @Column(nullable = false)
+    String status;
+    @Column(name = "created_at", nullable = false)
+    LocalDateTime createdAt;
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 
 }
--- src/main/java/com/aleprimo/nova_store/models/OrderItem.java
+++ src/main/java/com/aleprimo/nova_store/models/OrderItem.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table(name = "order_items")
 @Getter
@@ -18,4 +20,28 @@ public class OrderItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "order_id")
+    Order order;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "product_id")
+    Product product;
+
+    @Column(nullable = false)
+    Integer quantity;
+
+    @Column(name="unit_price", nullable = false)
+    BigDecimal unitPrice;
+
+    @Column(nullable = false)
+    BigDecimal subtotal;
+
+    @PrePersist
+    @PreUpdate
+    public void calculateSubtotal() {
+        if (unitPrice != null && quantity != null) {
+            subtotal = unitPrice.multiply(BigDecimal.valueOf(quantity));
+        }
+    }
 }
