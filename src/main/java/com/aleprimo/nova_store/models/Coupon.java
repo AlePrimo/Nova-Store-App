eecde731557d44aa67d44597190cacaad4a30f43
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-08-06 20:25:20 -0300

    Agregado de clase Coupon(descuento)

---
+++ src/main/java/com/aleprimo/nova_store/models/Coupon.java
@@ -0,0 +1,31 @@
+package com.aleprimo.nova_store.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "coupons")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private Double discountPercentage;
+
+    @Column(nullable = false)
+    private LocalDateTime expirationDate;
+
+    @Column(nullable = false)
+    private Boolean isActive;
+}
