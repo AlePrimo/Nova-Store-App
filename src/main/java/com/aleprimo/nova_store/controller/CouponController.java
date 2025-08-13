61bb74e764e60586459c3fe88e70832afbda9cd6
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-08-12 22:38:59 -0300

    Actualizacion

--- src/main/java/com/aleprimo/nova_store/controller/CouponController.java
+++ src/main/java/com/aleprimo/nova_store/controller/CouponController.java
@@ -3,6 +3,7 @@ package com.aleprimo.nova_store.controller;
 import com.aleprimo.nova_store.dto.coupon.CouponRequestDTO;
 import com.aleprimo.nova_store.dto.coupon.CouponResponseDTO;
 import com.aleprimo.nova_store.entityServices.CouponService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,34 +16,39 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/coupons")
 @RequiredArgsConstructor
-@Tag(name = "🧾 Cupones", description = "CRUD para gestión de cupones")
+@Tag(name = "Controlador de  Cupones", description = "CRUD para gestión de cupones")
 public class CouponController {
 
     private final CouponService couponService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Crear un nuevo cupon")
     public CouponResponseDTO create(@Valid @RequestBody CouponRequestDTO dto) {
         return couponService.create(dto);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener un cupon por su Id")
     public ResponseEntity<CouponResponseDTO> getById(@PathVariable Long id) {
         return ResponseEntity.ok(couponService.getById(id));
     }
 
     @GetMapping("/code/{code}")
+    @Operation(summary = "Obtener un cupon por su codigo")
     public ResponseEntity<CouponResponseDTO> getByCode(@PathVariable String code) {
         return ResponseEntity.ok(couponService.getByCode(code));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Borrar un cupon")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         couponService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping
+    @Operation(summary = "Obtener todos los cupones(con paginacion)")
     public ResponseEntity<Page<CouponResponseDTO>> getAll(Pageable pageable) {
         return ResponseEntity.ok(couponService.getAll(pageable));
     }
